d80a3cc12cc00a4e72e9caf4cbaec8759445fb6e
andreluismiller <46939182+andreluismiller@users.noreply.github.com>
2022-04-20 20:33:48 -0300

    home body widget commit

--- pi_7/lib/main.dart
+++ pi_7/lib/main.dart
@@ -1,18 +1,14 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 // import 'home.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage()
-    )
-  );
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -22,80 +18,107 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         backgroundColor: Colors.lightGreen,
         title: Text('Compras por NFC'),
         //'actions: [
-        //IconButton(icon: Icon(Icons.nome, color: Colors.cor,), onPressed: (){}) >>> Adiciona ícone com funcionalidade 
-        ),
+        //IconButton(icon: Icon(Icons.nome, color: Colors.cor,), onPressed: (){}) >>> Adiciona ícone com funcionalidade
+      ),
       drawer: Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text('Usuário'), 
-              accountEmail: Text('usuaro@email.com.br'),
+          child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Usuário'),
+            accountEmail: Text('usuaro@email.com.br'),
             currentAccountPicture: GestureDetector(
               child: CircleAvatar(
                 backgroundColor: Colors.grey,
                 child: Icon(
-                  Icons.person, color: Colors.black,
-                ),              
+                  Icons.person,
+                  color: Colors.black,
+                ),
               ),
             ),
             decoration: BoxDecoration(
               color: Colors.purple,
             ),
-            ),
-
-
-
-
-            //MENU LATERAL (navegação) do aplicativo
-            //CÓDIGO CORPO DO USERACCOUNTSDRAWERHEADER (barra lateral do app)
-          InkWell (              
-            onTap: (){}, //Adicionar função
+          ),
+          //MENU LATERAL (navegação) do aplicativo
+          //CÓDIGO CORPO DO USERACCOUNTSDRAWERHEADER (barra lateral do app)
+          InkWell(
+            onTap: () {}, //Adicionar função
             //Widget para transformar a entrada estática do menu em uma ação
             child: ListTile(
               title: Text('Página Inicial'),
               leading: Icon(Icons.home),
             ),
           ),
-
-            InkWell (
-              onTap: (){}, //Adicionar função
-              //Widget para transformar a entrada estática do menu em uma ação 
-              child: ListTile(
-                title: Text('Minha Lista'),
-                leading: Icon(Icons.shopping_basket),
-              ),
+          InkWell(
+            onTap: () {}, //Adicionar função
+            //Widget para transformar a entrada estática do menu em uma ação
+            child: ListTile(
+              title: Text('Minha Lista'),
+              leading: Icon(Icons.shopping_basket),
             ),
+          ),
 
-            Divider(),
+          Divider(),
 
-            InkWell (
-              onTap: (){}, //Adicionar função
-              //Widget para transformar a entrada estática do menu em uma ação              
-              child: ListTile(
-                title: Text('Configurações'),
-                leading: Icon(Icons.settings),
-              ),
+          InkWell(
+            onTap: () {}, //Adicionar função
+            //Widget para transformar a entrada estática do menu em uma ação
+            child: ListTile(
+              title: Text('Configurações'),
+              leading: Icon(Icons.settings),
             ),
-
-            InkWell ( 
-              onTap: (){}, //Adicionar função
-              //Widget para transformar a entrada estática do menu em uma ação
-              child: ListTile(
-                title: Text('Sobre'),
-                leading: Icon(Icons.help),
-              ),
+          ),
+          InkWell(
+            onTap: () {}, //Adicionar função
+            //Widget para transformar a entrada estática do menu em uma ação
+            child: ListTile(
+              title: Text('Sobre'),
+              leading: Icon(Icons.help),
             ),
-          
-          ],
-        )
+          ),
+        ],
+      )),
+
+
+      //SEÇÃO CORPO
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text('Seja bem-vindo'),
+                Text('Nome usuário'),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text('Vamos as compras?'),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('Ativar NFC'),
+                  //TO DO:
+                  //Implementar função para ativar NFC do dispositivo
+                ),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('Sair do app'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
 
 //EXERCÍCIO - IGNORAR
 // void main() {
@@ -119,15 +142,13 @@ class _HomePageState extends State<HomePage> {
 //   );
 // }
 
-
-
 // //HOMEPAGE
 // void main() {
 //   runApp(MyApp());
 // }
 
 // class MyApp extends StatelessWidget {
-    //StatelessWidget são aqueles que não alteram o conteúdo da página, mesmo diante de input do usuário (ex: login, )
+//StatelessWidget são aqueles que não alteram o conteúdo da página, mesmo diante de input do usuário (ex: login, )
 //   @override
 //   Widget build(BuildContext context) {
 //     var texto = ['Seja bem-vindo(a),', 'Nome do usuário'];
@@ -144,7 +165,7 @@ class _HomePageState extends State<HomePage> {
 //                 children: [
 //                   Text('Seja bem-vindo'),
 //                   Text('Nome usuário'),
-//                 ],              
+//                 ],
 //               ),
 //             ),
 //             Container(
@@ -155,7 +176,7 @@ class _HomePageState extends State<HomePage> {
 //                   ElevatedButton(
 //                     onPressed: null,
 //                     child: Text('Ativar NFC'),
-//                     //TO DO: 
+//                     //TO DO:
 //                     //Implementar função para ativar NFC do dispositivo
 //                   ),
 //                   ElevatedButton(
@@ -172,13 +193,10 @@ class _HomePageState extends State<HomePage> {
 //   }
 // }
 
-
 //TO DO:
 //Criar janela pop-up ativada pela detecção de uma etiqueta NFC, contendo as informações do produto
-//(tipo, marca, quantidade e valor) = [{"LanguageCode":"English","Text":"(tipo, marca, quantidade e valor","EncodingType":"UTF8","RecordType":"Text"}], 
+//(tipo, marca, quantidade e valor) = [{"LanguageCode":"English","Text":"(tipo, marca, quantidade e valor","EncodingType":"UTF8","RecordType":"Text"}],
 //com opções para adicionar produto à lista ou voltar a escanear produtos
-
-
 
 //CRIAÇÃO DE PÁGINA: Lista de compras
 //Espécie de carrinho em forma de lista com todos os produtos adicionados pelo usuário e a somatória total dos produtos
